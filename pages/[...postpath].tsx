8390f5093e5035facc0487cc93e1b173826bf3a8
JJTikTok <kiransalman271@gmail.com>
2026-06-05 02:56:15 +0500

    [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,114 +1,63 @@
-import React from "react";
-import Head from "next/head";
-import { GetServerSideProps } from "next";
-import { GraphQLClient, gql } from "graphql-request";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-  const graphQLClient = new GraphQLClient(endpoint);
-  const referringURL = ctx.req.headers?.referer || null;
-  const pathArr = ctx.query.postpath as Array<string>;
-  const path = pathArr.join("/");
-  console.log(path);
-  const fbclid = ctx.query.fbclid;
-  console.log(referringURL);
+const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT as string
 
-  // redirect if facebook is the referer or request contains fbclid
-  if (referringURL?.includes("facebook.com") || fbclid) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: `${
-          endpoint.replace(/(\/graphql\/)/, "/") + encodeURI(path as string)
-        }`,
-      },
-    };
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { postpath } = req.query
+  const slugArr = Array.isArray(postpath) ? postpath : [postpath || '']
+  const slugPath = slugArr.join('/')
+
+  const ua = req.headers['user-agent'] || ''
+  const isFbBot =
+    ua.indexOf('facebookexternalhit') > -1 || ua.indexOf('Facebot') > -1
+
+  const endpoint = GRAPHQL_ENDPOINT
+  const wpBase = endpoint.replace('/graphql/', '').replace('/graphql', '')
+  const redirectUrl = wpBase + '/' + slugPath + '/'
+
+  if (!isFbBot) {
+    res.redirect(307, redirectUrl)
+    return
   }
-  const query = gql`
-		{
-			post(id: "/${path}/", idType: URI) {
-				id
-				excerpt
-				title
-				link
-				dateGmt
-				modifiedGmt
-				content
-				author {
-					node {
-						name
-					}
-				}
-				featuredImage {
-					node {
-						sourceUrl
-						altText
-					}
-				}
-			}
-		}
-	`;
 
-  const data = await graphQLClient.request(query);
-  if (!data.post) {
-    return {
-      notFound: true,
-    };
+  try {
+    const graphRes = await fetch(endpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query:
+          'query($slug: String!) { postBy(slug: $slug) { title excerpt featuredImage { node { sourceUrl } } } }',
+        variables: { slug: slugPath },
+      }),
+    })
+    const json = await graphRes.json()
+    const post = json && json.data ? json.data.postBy : null
+
+    const title = post && post.title ? post.title : ''
+    const desc =
+      post && post.excerpt ? post.excerpt.replace(/<[^>]*>/g, '') : ''
+    const image =
+      post && post.featuredImage && post.featuredImage.node
+        ? post.featuredImage.node.sourceUrl
+        : ''
+
+    const html = `<!DOCTYPE html>
+<html>
+<head>
+<title>${title}</title>
+<meta property="og:title" content="${title}" />
+<meta property="og:description" content="${desc}" />
+<meta property="og:url" content="${redirectUrl}" />
+<meta property="og:type" content="article" />
+${image ? `<meta property="og:image" content="${image}" />` : ''}
+<meta http-equiv="refresh" content="0;url=${redirectUrl}" />
+</head>
+<body><p>Redirecting...</p></body>
+</html>`
+
+    res.setHeader('Content-Type', 'text/html')
+    res.status(200).send(html)
+  } catch (e) {
+    res.redirect(307, redirectUrl)
   }
-  return {
-    props: {
-      path,
-      post: data.post,
-      host: ctx.req.headers.host,
-    },
-  };
-};
-
-interface PostProps {
-  post: any;
-  host: string;
-  path: string;
 }
-
-const Post: React.FC<PostProps> = (props) => {
-  const { post, host, path } = props;
-
-  // to remove tags from excerpt
-  const removeTags = (str: string) => {
-    if (str === null || str === "") return "";
-    else str = str.toString();
-    return str.replace(/(<([^>]+)>)/gi, "").replace(/\[[^\]]*\]/, "");
-  };
-
-  return (
-    <>
-      <Head>
-        <meta property="og:title" content={post.title} />
-        <link rel="canonical" href={`https://${host}/${path}`} />
-        <meta property="og:description" content={removeTags(post.excerpt)} />
-        <meta property="og:url" content={`https://${host}/${path}`} />
-        <meta property="og:type" content="article" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:site_name" content={host.split(".")[0]} />
-        <meta property="article:published_time" content={post.dateGmt} />
-        <meta property="article:modified_time" content={post.modifiedGmt} />
-        <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-        <meta
-          property="og:image:alt"
-          content={post.featuredImage.node.altText || post.title}
-        />
-        <title>{post.title}</title>
-      </Head>
-      <div className="post-container">
-        <h1>{post.title}</h1>
-        <img
-          src={post.featuredImage.node.sourceUrl}
-          alt={post.featuredImage.node.altText || post.title}
-        />
-        <article dangerouslySetInnerHTML={{ __html: post.content }} />
-      </div>
-    </>
-  );
-};
-
-export default Post;
