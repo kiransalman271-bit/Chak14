4326dc0271d6b214961bdd96e851ff3ef628b885
JJTikTok <kiransalman271@gmail.com>
2026-06-05 02:50:25 +0500

    pages/[...slug].tsx

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,71 +1,82 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
-const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT as string
-
-interface Post {
-  title: string
-  excerpt: string
-  link: string
-  featuredImage: { node: { sourceUrl: string } } | null
-}
-
 interface Props {
-  post: Post | null
+  title: string
+  description: string
+  image: string
   redirectUrl: string
 }
 
-export default function Page({ post, redirectUrl }: Props) {
-  const description = post?.excerpt?.replace(/<[^>]*>/g, '') || ''
+export default function Page({ title, description, image, redirectUrl }: Props) {
   return (
     <>
       <Head>
-        <title>{post?.title || 'Redirecting...'}</title>
-        <meta property="og:title" content={post?.title || ''} />
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:url" content={redirectUrl} />
         <meta property="og:type" content="article" />
-        {post?.featuredImage && (
-          <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-        )}
-        <meta httpEquiv="refresh" content={`0;url=${redirectUrl}`} />
+        {image ? <meta property="og:image" content={image} /> : null}
+        <meta httpEquiv="refresh" content={'0;url=' + redirectUrl} />
       </Head>
-      <p>Redirecting to article...</p>
+      <p>Redirecting...</p>
     </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const slugArr = ctx.params?.slug as string[]
+  const params = ctx.params
+  const slugArr = params && params.slug ? (params.slug as string[]) : []
   const slugPath = slugArr.join('/')
   const ua = ctx.req.headers['user-agent'] || ''
-  const isFbBot = ua.includes('facebookexternalhit') || ua.includes('Facebot')
+  const isFbBot =
+    ua.indexOf('facebookexternalhit') > -1 || ua.indexOf('Facebot') > -1
 
-  const wpBase = GRAPHQL_ENDPOINT.replace(/\/graphql\/?$/, '')
-  const redirectUrl = `${wpBase}/${slugPath}/`
+  const endpoint = process.env.GRAPHQL_ENDPOINT || ''
+  const wpBase = endpoint.replace('/graphql/', '').replace('/graphql', '')
+  const redirectUrl = wpBase + '/' + slugPath + '/'
 
   if (!isFbBot) {
-    return { redirect: { destination: redirectUrl, permanent: false } }
+    return {
+      redirect: {
+        destination: redirectUrl,
+        permanent: false,
+      },
+    }
   }
 
+  let title = ''
+  let description = ''
+  let image = ''
+
   try {
-    const res = await fetch(GRAPHQL_ENDPOINT, {
+    const res = await fetch(endpoint, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        query: `query($slug: String!) {
-          postBy(slug: $slug) {
-            title excerpt link
-            featuredImage { node { sourceUrl } }
-          }
-        }`,
+        query:
+          'query($slug: String!) { postBy(slug: $slug) { title excerpt featuredImage { node { sourceUrl } } } }',
         variables: { slug: slugPath },
       }),
     })
     const json = await res.json()
-    const post: Post = json?.data?.postBy || null
-    return { props: { post, redirectUrl } }
-  } catch {
-    return { props: { post: null, redirectUrl } }
+    const post = json && json.data && json.data.postBy ? json.data.postBy : null
+    if (post) {
+      title = post.title || ''
+      description = post.excerpt
+        ? post.excerpt.replace(/<[^>]*>/g, '')
+        : ''
+      image =
+        post.featuredImage && post.featuredImage.node
+          ? post.featuredImage.node.sourceUrl
+          : ''
+    }
+  } catch (e) {
+    console.log('GraphQL fetch error', e)
+  }
+
+  return {
+    props: { title, description, image, redirectUrl },
   }
 }
